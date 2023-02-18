e87c9e22dbbb79d32dfc69f198763d1cc1d2de56
Ridowan Sajid <83457290+Ridowan-sajid@users.noreply.github.com>
2023-02-18 16:51:01 +0600

    Update 18.Lab-4-2.asm

--- 18.Lab-4-2.asm
+++ 18.Lab-4-2.asm
@@ -1,40 +1,26 @@
-                      ;include "EMU8086.INC"
 .model small
 .stack 100h 
-.data
 
-m db "AL greater than 0$"
-m2 db "AL less than 0$"
 .code
 
 main proc
-    mov ax,@data
-    mov ds, ax
+
     
     mov al,-4 
     mov ah,0 
     
     
-    cmp al,0 ;ax<0
+    cmp al,0 
     jl last 
     
-    mov ah,15h
-    
-    mov ah,9 
-    lea dx,m
-    int 21h
+    mov ah,0
     jmp end_ 
     
     
     last: 
-    mov bx,0
-    
-    mov ah,9 
-    lea dx,m2
-    int 21h
+    mov ah,11111111b
     
    end_:
     mov ah,4ch
     int 21h
     main endp
-
